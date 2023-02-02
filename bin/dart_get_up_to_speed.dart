2204d062394bd610d0425e8a497b7103efadda17
thephotogenicbug <navinkumarachari@gmail.com>
2023-02-02 23:29:44 +0530

    Dart bootcamp

---
+++ bin/dart_get_up_to_speed.dart
@@ -0,0 +1,5 @@
+import 'package:dart_get_up_to_speed/dart_get_up_to_speed.dart' as dart_get_up_to_speed;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_get_up_to_speed.calculate()}!');
+}
